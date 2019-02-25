1640d90346c46bc38074486b53029589e8284c57
Sebastian Luna-Valero <sebastian.luna.valero@gmail.com>
2019-02-25 14:56:21 +0100

    bugfix run.sh

--- run.sh
+++ run.sh
@@ -4,22 +4,24 @@
 
 [[ -r Miniconda3-latest-Linux-x86_64.sh ]] || curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 [[ -d conda-install ]] || bash Miniconda3-latest-Linux-x86_64.sh -b -p conda-install
-[[ `which conda` ]] || (source conda-install/etc/profile.d/conda.sh && conda update --all --yes)
+
+if [[ ! `which conda` ]] ; then
+    source conda-install/etc/profile.d/conda.sh && \
+    conda update --all --yes
+fi
 
 ### Install cgatcore
 
-[[ `conda env list | grep cgatcore | grep '*'` ]] || \
-    (conda create --name cgatcore cgatcore --channel bioconda --channel conda-forge --yes && \
-     conda activate cgatcore)
+if [[ ! `conda env list | grep cgatcore | grep '*'` ]] ; then
+    conda create --name cgatcore cgatcore --channel bioconda --channel conda-forge --yes && \
+    conda activate cgatcore
+fi
 
 ### Download pipeline
 
-[[ -d hcg-16-master/ ]] || \
-    (wget https://github.com/AMIGA-IAA/hcg-16/archive/master.zip && \
-     unzip master.zip && \
-     rm master.zip)
+[[ -r pipeline.py ]] || curl -O https://raw.githubusercontent.com/AMIGA-IAA/hcg-16/master/cgatcore/pipeline.py
 
 ### Run pipeline
 
-python hcg-16-master/cgatcore/pipeline.py make masking --local --timeit=pipeline.time --shell-logfile=pipeline.shell
+python pipeline.py make masking --local --timeit=pipeline.time --shell-logfile=pipeline.shell
 
