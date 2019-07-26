2ce42479fee9bb6033683b216427c2ab49c326e7
mike <mike.jones.astro@gmail.com>
2019-07-26 11:49:47 +0200

    Split the cube in channel maps generation into two halves to prevent notebook stalling.

--- plot_scripts/FigC1-C2-Channel_maps.py
+++ plot_scripts/FigC1-C2-Channel_maps.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
+
 # coding: utf-8
 
 # In[ ]:
-
 
 import matplotlib,aplpy
 from astropy.io import fits
@@ -12,7 +11,6 @@ from astropy.wcs import WCS
 
 
 # In[ ]:
-
 
 font = {'size'   : 14, 'family' : 'serif', 'serif' : 'cm'}
 plt.rc('font', **font)
@@ -28,7 +26,6 @@ save_figs = True
 
 # In[ ]:
 
-
 r_image_decals = 'HCG16_DECaLS_r_cutout.fits'
 grz_image_decals = 'HCG16_DECaLS_cutout.jpeg'
 cube_casa = 'HCG16_CD_rob2_MS.pbcor.fits'
@@ -41,16 +38,17 @@ cube_casa = 'HCG16_CD_rob2_MS.pbcor.fits'
 # 
 # 3. The primary beam corrected, multi-scale CLEAN HI cube generated in the *imaging* step using CASA. The script [imaging.py](casa/imaging.py) contains the commands used to generate it.
 
-# First make channel maps in the group centre.
+# First make channel maps in the group centre. 
+# 
+# It is necessary to run through the cube in two halves to prevent the notebook from stalling.
 
 # In[ ]:
-
 
 #Load the cube
 cube,cube_ra,cube_dec,cube_vel = read_fitscube(cube_casa)
 
 #Make map for each channel
-for i in range(len(cube_vel)):
+for i in range(len(cube_vel)/2):
     #Initialise background figure
     f = aplpy.FITSFigure(r_image_decals,figsize=(12,9),dimensions=[0,1])
 
@@ -84,18 +82,64 @@ for i in range(len(cube_vel)):
     #Save
     if save_figs:
         f.savefig('FigC1-HCG16_core_chnmap_'+str(i)+'.pdf')
+        
+    #Close figure
+    f.close()
+
+
+# In[ ]:
+
+#Now run the second half of the cube
+
+#Make map for each channel
+for i in range(len(cube_vel)/2,len(cube_vel)):
+    #Initialise background figure
+    f = aplpy.FITSFigure(r_image_decals,figsize=(12,9),dimensions=[0,1])
+
+    f.recenter(32.385,  -10.1584, height=9./60., width=12./60.)
+
+    #Aplpy cannot properly read the header and slice the cube
+    #So we have to read it with astropy and pass it to aplpy
+    hdu = fits.open(cube_casa)
+    wcs = WCS(hdu[0].header,naxis=2)
+    data = hdu[0].data[i]
+    hdu = fits.PrimaryHDU(data)
+    hdu.header.update(wcs.to_header())
+    
+    #Overlay HI channel contours
+    f.show_contour(hdu,
+                   colors=['black'],levels=numpy.array([-2])*0.45E-3,linestyle='--')
+    f.show_contour(hdu,
+                   colors=['black','blue','purple','red','darkorange'],levels=numpy.array([2,4,8,16,32])*0.45E-3)
+
+    #Set colour stretch of background image
+    f.show_colorscale(cmap='Greys',vmin=0.0001,vmax=5.,stretch='log')
+    
+    #Add grid lines
+    f.add_grid()
+    f.grid.set_color('black')
+    
+    #Add velcoity label
+    f.add_label(0.85,0.95,str(int(numpy.round(cube_vel[i],0)))+' km/s',horizontalalignment='left',
+                relative='axes', color='r', size='large')
+
+    #Save
+    if save_figs:
+        f.savefig('FigC1-HCG16_core_chnmap_'+str(i)+'.pdf')
+        
+    #Close figure
+    f.close()
 
 
 # Now make channel maps near the SE tail.
 
 # In[ ]:
 
-
 #Load the cube
 cube,cube_ra,cube_dec,cube_vel = read_fitscube(cube_casa)
 
 #Make map for each channel
-for i in range(len(cube_vel)):
+for i in range(len(cube_vel)/2):
     #Initialise background figure
     f = aplpy.FITSFigure(r_image_decals,figsize=(14,12),dimensions=[0,1])
 
@@ -129,10 +173,51 @@ for i in range(len(cube_vel)):
     #Save
     if save_figs:
         f.savefig('FigC2-HCG16_tail_chnmap_'+str(i)+'.pdf')
+        
+    #Close figure
+    f.close()
 
 
 # In[ ]:
 
+#Now run the second half of the cube
 
+#Make map for each channel
+for i in range(len(cube_vel)/2,len(cube_vel)):
+    #Initialise background figure
+    f = aplpy.FITSFigure(r_image_decals,figsize=(14,12),dimensions=[0,1])
 
+    f.recenter(32.52,  -10.27, height=12./60., width=14./60.)
+
+    #Aplpy cannot properly read the header and slice the cube
+    #So we have to read it with astropy and pass it to aplpy
+    hdu = fits.open(cube_casa)
+    wcs = WCS(hdu[0].header,naxis=2)
+    data = hdu[0].data[i]
+    hdu = fits.PrimaryHDU(data)
+    hdu.header.update(wcs.to_header())
+    
+    #Overlay HI channel contours
+    f.show_contour(hdu,
+                   colors=['black'],levels=numpy.array([-2])*0.45E-3,linestyle='--')
+    f.show_contour(hdu,
+                   colors=['black','blue','purple','red','darkorange'],levels=numpy.array([2,4,8,16,32])*0.45E-3)
+
+    #Set colour stretch of background image
+    f.show_colorscale(cmap='Greys',vmin=0.0001,vmax=5.,stretch='log')
+    
+    #Add grid lines
+    f.add_grid()
+    f.grid.set_color('black')
+    
+    #Add velcoity label
+    f.add_label(0.85,0.05,str(int(numpy.round(cube_vel[i],0)))+' km/s',horizontalalignment='left',
+                relative='axes', color='r', size='large')
+
+    #Save
+    if save_figs:
+        f.savefig('FigC2-HCG16_tail_chnmap_'+str(i)+'.pdf')
+        
+    #Close figure
+    f.close()
 
