ce7a502145426dcf04e72b75bfa346ef535fa652
mike <mike.jones.astro@gmail.com>
2019-09-19 12:20:50 +0200

    Removed redundant plot scripts and fixed some path problems.

--- plot_scripts/Fig4-Integrated_spectrum.py
+++ plot_scripts/Fig4-Integrated_spectrum.py
@@ -32,13 +32,17 @@ save_figs = True
 mask_sofia = 'HCG16_CD_rob2_MS_mask.fits'
 cube_casa = 'HCG16_CD_rob2_MS.pbcor.fits'
 spec_gbt = 'GBT_HCG16_spec.ascii'
-spec_hipass = 'HCG16-spectrum.11x11.log'
+cube_hipass = 'H290_abcde_luther_chop.fits'
+mask_sofia_hipass = 'HIPASS_cube_sofia_mask.fits'
+sofia_params_hipass = 'HIPASS_cube_params.session'
 
 
 # 1. A $3.5\sigma$ mask of HCG 16 made with SoFiA after smoothing over various kernel sizes. This file was generated in the *masking* step of the workflow. The SoFiA parameters file which makes this file is [HCG16_CD_rob2_MS.3.5s.dil.session](sofia/HCG16_CD_rob2_MS.3.5s.dil.session).
 # 2. The primary beam corrected, multi-scale CLEAN HI cube generated in the *imaging* step using CASA. The script [imaging.py](casa/imaging.py) contains the commands used to generate it.
 # 3. The Green Bank Telescope spectrum of HCG 16 from the paper [Borthakur et al. 2010](iopscience.iop.org/article/10.1088/0004-637X/710/1/385/meta). Provided by S. Borthakur.
-# 4. The HIPASS (HI Parkes All Sky Survey) spectrum of HCG 16. Provided by B. Koribalski (private communication).
+# 4. The HIPASS ([HI Parkes All Sky Survey](https://www.atnf.csiro.au/research/multibeam/release/)) cube covering the region of HCG 16 [(Barnes et al. 2001)](https://academic.oup.com/mnras/article/322/3/486/952500).
+# 5. The SoFiA-generated mask of covering the HIPASS sub-cube containing HCG 16. This file was generated in the *masking* step of the workflow. The SoFiA parameters file which makes this file is [HIPASS_cube_params.session](sofia/HIPASS_cube_params.session).
+# 6. The SoFiA parameters file for the mask (item 5).
 
 # The mask and the cube need to be combined to eliminate regions of the cube which only contribute noise. Then the spatial dimensions of the cube must be collapsed to make a 1-dimensional spectrum.
 
@@ -46,7 +50,7 @@ spec_hipass = 'HCG16-spectrum.11x11.log'
 
 
 #Read in the SoFiA mask
-mask,mask_ra,mask_dec,mask_vel = read_fitscube(mask_sofia)
+mask,mask_ra,mask_dec,mask_vel = read_fitscube(mask_sofia,mask=True)
 #Read in the primary beam corrected HI cube
 cube,cube_ra,cube_dec,cube_vel,bmaj,bmin,pa,beam_factor,cube_dx,cube_dy,cube_dv = read_fitscube(cube_casa,True,True)
 #Mask the cube
@@ -93,12 +97,31 @@ for i in range(leni):
 VLA_spec_wt = numpy.nansum(numpy.nansum(masked_cube*pix_wts,axis=1),axis=1)/beam_factor
 
 
-# Read in the HIPASS spectrum. This covers the whole field so no weighting is required.
+# Read in the HIPASS cube, mask it and collapse it to form the spectrum.
 
 # In[ ]:
 
 
-HIP_spec = pandas.read_csv(spec_hipass,skiprows=6,names=['Channel','Vel','Flux'],delim_whitespace=True)
+#Read in the SoFiA mask for the HIPASS cube
+HIP_mask,HIP_mask_ra,HIP_mask_dec,HIP_mask_vel = read_fitscube(mask_sofia_hipass,mask=True)
+#Read in the HIPASS cube
+HIP_cube,HIP_cube_ra,HIP_cube_dec,HIP_cube_vel,HIP_bmaj,HIP_bmin,HIP_pa,HIP_beam_factor,HIP_cube_dx,HIP_cube_dy,HIP_cube_dv = read_fitscube(cube_hipass,True,True)
+
+#Read parameters file from SoFiA execution
+#Find definition of the sub-region of the cube
+param_file = open(sofia_params_hipass,'r')
+for line in param_file:
+    if 'import.subcube\t' in line:
+        box = line.split('=')[1]
+        box = box[2:-2]
+        box = numpy.array(box.split(','),dtype='int')
+param_file.close()
+
+#Mask the sub-cube
+HIP_masked_subcube = numpy.multiply(HIP_mask,HIP_cube[box[4]:box[5],box[2]:box[3],box[0]:box[1]])
+
+#Sum over the spatial dimensions to generate the integrated spectrum
+HIP_cube_spec = numpy.nansum(numpy.nansum(HIP_masked_subcube,axis=1),axis=1)/HIP_beam_factor
 
 
 # Finally generate the plot.
@@ -111,7 +134,7 @@ fig = plt.subplots(figsize=(8.27,2.*3))
 
 #Plot the GBT and HIPASS spectra
 plt.step(GBT_spec['vel'],GBT_spec['flux'],c='g',where='mid',label='GBT')
-plt.plot(HIP_spec['Vel']+0.5*(HIP_spec['Vel'][1]-HIP_spec['Vel'][0]),HIP_spec['Flux'],c='orange',label='HIPASS',drawstyle='steps')
+plt.plot(HIP_mask_vel,HIP_cube_spec,lw=1,ls='-',c='orange',drawstyle='steps-mid',label='HIPASS')
 
 #Plot the VLA spectra
 plt.step(cube_vel,VLA_spec,c='k',lw=2,where='mid',label='VLA')
@@ -131,13 +154,13 @@ plt.axvline(v_hcg16n,ls='--',c='k')
 plt.axvline(v_hcg16p,ls='--',c='k')
 
 #Annotate the lines
-plt.annotate('HCG 16c',[v_hcg16c-165.,0.175],fontsize='14')
-plt.annotate('HCG 16b',[v_hcg16c-165.,0.165],fontsize='14')
-plt.annotate('HCG 16d',[v_hcg16c-165.,0.155],fontsize='14')
+plt.annotate('HCG 16c',[v_hcg16c-165.,0.195],fontsize='14')
+plt.annotate('HCG 16b',[v_hcg16c-165.,0.185],fontsize='14')
+plt.annotate('HCG 16d',[v_hcg16c-165.,0.175],fontsize='14')
 
-plt.plot([v_hcg16c-25.,v_hcg16c],[0.177,0.17],c='k')
-plt.plot([v_hcg16c-25.,v_hcg16b],[0.167,0.16],c='k')
-plt.plot([v_hcg16c-25.,v_hcg16d],[0.157,0.15],c='k')
+plt.plot([v_hcg16c-25.,v_hcg16c],[0.197,0.21],c='k')
+plt.plot([v_hcg16c-25.,v_hcg16b],[0.187,0.20],c='k')
+plt.plot([v_hcg16c-25.,v_hcg16d],[0.177,0.19],c='k')
 
 plt.annotate('PGC 8210',[4120.,0.09],fontsize='14')
 plt.annotate('NGC 848',[4120.,0.08],fontsize='14')
